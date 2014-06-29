654bb174b956272c604c83c927a54e784dc22635
Eugen Richter <android@webducer.de>
2014-06-29 12:14:43 +0200

    Hausaufgabe für den Tag 3

--- Zeiterfassung/src/de/mvhs/android/zeiterfassung/ZeitApp.java
+++ Zeiterfassung/src/de/mvhs/android/zeiterfassung/ZeitApp.java
@@ -7,12 +7,19 @@ import org.acra.annotation.ReportsCrashes;
 import android.app.Application;
 
 @ReportsCrashes(formKey = "", // Kompabilität
-mailTo = "bugs@meineapp.de", mode = ReportingInteractionMode.TOAST, resToastText = R.string.acra_toast_crash)
+mailTo = "bugs@meineapp.de", mode = ReportingInteractionMode.DIALOG, resToastText = R.string.crash_toast_text, // optional, displayed as soon as the crash
+                                                                                                               // occurs, before collecting data which can take
+                                                                                                               // a few seconds
+resDialogText = R.string.crash_dialog_text, resDialogIcon = android.R.drawable.ic_dialog_info, // optional. default is a warning sign
+resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
+resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
+resDialogOkToast = R.string.crash_dialog_ok_toast // optional. displays a Toast message when the user accepts to send a report.
+)
 public class ZeitApp extends Application {
-	@Override
-	public void onCreate() {
-		super.onCreate();
+  @Override
+  public void onCreate() {
+    super.onCreate();
 
-		ACRA.init(this);
-	}
+    ACRA.init(this);
+  }
 }
