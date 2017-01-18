764bfe0bc42feb637d532c28023387abb510043c
Your Name <you@example.com>
2017-01-18 19:40:22 +0100

    crashreport 2 über annotation

--- app/src/main/java/de/mvhs/android/zeiterfassung/TimeTrackingApp.java
+++ app/src/main/java/de/mvhs/android/zeiterfassung/TimeTrackingApp.java
@@ -6,6 +6,7 @@ import android.content.Context;
 
 import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
 import org.acra.config.ACRAConfiguration;
 import org.acra.config.ACRAConfigurationException;
 import org.acra.config.ConfigurationBuilder;
@@ -14,18 +15,20 @@ import org.acra.config.ConfigurationBuilder;
  * Created by javadev on 18.01.17.
  */
 
-
+@ReportsCrashes(mailTo = "big.sexy@o2online.de",
+mode = ReportingInteractionMode.DIALOG,
+resDialogText = R.string.CrashDialogText,
+resDialogTitle = R.string.CrashDialogTitle,
+resDialogTheme = R.style.AppTheme)
 public final class TimeTrackingApp extends Application {
     //Acra.ch
-
 
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
 
-
     // init über code
-
+/*
         try {
             final ACRAConfiguration config = new ConfigurationBuilder(this)
                         .setMailTo("big.sexy@o2online.de")
@@ -40,8 +43,12 @@ public final class TimeTrackingApp extends Application {
         } catch (ACRAConfigurationException e) {
             e.printStackTrace();
         }
-    }
 
+*/
+
+    ACRA.init(this ); // rest über die annotation
+
+    }
     @Override
     public void onTerminate() {
         super.onTerminate();
